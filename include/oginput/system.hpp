ef23341a974fdffbfea044c6d6970775726b6588
Colleen <cdnickerson05@gmail.com>
2023-09-01 02:15:23 -0300

    defined system class

--- include/oginput/system.hpp
+++ include/oginput/system.hpp
@@ -7,12 +7,51 @@
 #ifndef OGI_SYSTEM_HPP
 #define OGI_SYSTEM_HPP
 
+#include "oginput/event.hpp"
 #include "oginput/action.hpp"
 #include "oginput/listener.hpp"
 
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <memory>
+
+#include <iostream>
+
 namespace ogi {
     class system {
+        private:
+            std::map<keyboard_key, bool> _lastKeyboardKeyStates;
+            std::map<mouse_button, bool> _lastMouseButtonStates;
+            std::map<device_id, std::map<gamepad_button, bool>>
+                _lastGamepadButtonStates;
+
+            std::map<keyboard_key, bool> _keyboardKeyStates;
+            std::map<mouse_button, bool> _mouseButtonStates;
+            std::map<device_id, std::map<gamepad_button, bool>>
+                _gamepadButtonStates;
+
+            std::map<mouse_axis, float> _mouseAxisStates;
+
+            std::map<device_id, std::map<gamepad_axis, float>> _gampeadAxisStates;
+
+            std::vector<std::shared_ptr<listener>> _actionListeners;
+            std::vector<size_t> _actionListenerGarbage;
+
+            void _clean_dead_listeners();
+
         public:
+            std::unordered_map<std::string, action_map> action_maps;
+
+            void register_event(keyboard_event event);
+            void register_event(mouse_event event);
+            void register_event(gamepad_event event);
+
+            void push_listener(std::shared_ptr<listener> listener);
+
+            void update();
+
             system();
             ~system();
     };
