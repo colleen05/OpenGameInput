d49818ca2e24cffdf9ef0cc078e006ca133af26a
Colleen <cdnickerson05@gmail.com>
2023-09-01 02:15:36 -0300

    defined listener class

--- include/oginput/listener.hpp
+++ include/oginput/listener.hpp
@@ -7,9 +7,31 @@
 #ifndef OGI_LISTENER_HPP
 #define OGI_LISTENER_HPP
 
+#include "oginput/event.hpp"
+#include "oginput/action.hpp"
+
+#include <unordered_map>
+#include <functional>
+#include <string>
+
 namespace ogi {
+    using button_callback = std::function<void(button_activity)>;
+    using scalar_callback = std::function<void(float)>;
+    using planar_callback = std::function<void(float, float)>;
+
+    using callback = std::variant<button_callback, scalar_callback, planar_callback>;
+
     class listener {
+        private:
+            std::unordered_map<std::string, callback> _callbacks;
+            
         public:
+            void set_callback(const std::string &action_name, callback callback_function);
+            void clear_callback(const std::string &action_name);
+            callback get_callback(const std::string &action_name);
+            void call(const std::string &action_name);
+
+            listener(std::unordered_map<std::string, callback> callbacks);
             listener();
             ~listener();
     };
